999d3dbdd6f90631cb79cc78c185c34039601dab
qqlidakai <qqlidakai@163.com>
2019-05-28 22:24:21 +0800

    练习冲突解决eclipse

--- Demo/src/cn/com/nantin/Demo3.java
+++ Demo/src/cn/com/nantin/Demo3.java
@@ -2,6 +2,6 @@ package cn.com.nantin;
 
 public class Demo3 {
 public static void main(String[] args) {
-	System.out.println("11111111111111eclipse");
+	System.out.println("211111111111111eclipse");
 }
 }
--- Demo/src/cn/com/nantin/Demo4.java
+++ Demo/src/cn/com/nantin/Demo4.java
@@ -2,6 +2,6 @@ package cn.com.nantin;
 
 public class Demo4 {
 	public static void main(String[] args) {
-		System.out.println("22222222eclipse");
+		System.out.println("122222222eclipse");
 	}
 }
