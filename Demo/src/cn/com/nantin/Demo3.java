3fc75f64190ca03c45b2b7a396ee3dcf4da4032b
qqlidakai <qqlidakai@163.com>
2019-05-28 22:21:32 +0800

    11

---
+++ Demo/src/cn/com/nantin/Demo3.java
@@ -0,0 +1,7 @@
+package cn.com.nantin;
+
+public class Demo3 {
+public static void main(String[] args) {
+	System.out.println("11111111111111eclipse");
+}
+}
---
+++ Demo/src/cn/com/nantin/Demo4.java
@@ -0,0 +1,7 @@
+package cn.com.nantin;
+
+public class Demo4 {
+	public static void main(String[] args) {
+		System.out.println("22222222eclipse");
+	}
+}
