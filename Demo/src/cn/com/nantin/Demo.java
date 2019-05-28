bf17af1d624ed703575e29a78aadb8441cf8e288
qqlidakai <qqlidakai@163.com>
2019-05-28 21:48:12 +0800

    提交代码

--- Demo/src/cn/com/nantin/Demo.java
+++ Demo/src/cn/com/nantin/Demo.java
@@ -6,6 +6,6 @@ public static void main(String[] args) {
 	System.out.println("diyigeyonghu");
     System.out.println("+++");
     System.out.println("--------");
-    System.out.println("ddsgdgjdlgj");
+
 }
 }
--- Demo/src/cn/com/nantin/Demo2.java
+++ Demo/src/cn/com/nantin/Demo2.java
@@ -2,7 +2,7 @@ package cn.com.nantin;
 
 public class Demo2 {
 	public static void main(String[] args) {
-		
+		System.out.println("--------");
 	}
 
 }
