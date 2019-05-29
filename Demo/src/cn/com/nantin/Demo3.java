6084adbfc93f83dd25a488885b01628efbb934a1
qqlidakai <qqlidakai@163.com>
2019-05-29 21:34:13 +0800

    idean

--- Demo/src/cn/com/nantin/Demo3.java
+++ Demo/src/cn/com/nantin/Demo3.java
@@ -2,7 +2,7 @@ package cn.com.nantin;
 
 public class Demo3 {
 public static void main(String[] args) {
-	System.out.println("333333idea");
+	System.out.println("idea");
 	System.out.println("111idea");
 }
 }
