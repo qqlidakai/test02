ca27914cfd87ccf70f68a1b4025b48063329fb04
qqlidakai <qqlidakai@163.com>
2019-05-29 21:48:50 +0800

    idea tijiao3333

--- Demo/src/cn/com/nantin/Demo3.java
+++ Demo/src/cn/com/nantin/Demo3.java
@@ -3,6 +3,6 @@ package cn.com.nantin;
 public class Demo3 {
 public static void main(String[] args) {
 	System.out.println("eclipse");
-	System.out.println("ecl");
+	System.out.println("ecl0000000");
 }
 }
