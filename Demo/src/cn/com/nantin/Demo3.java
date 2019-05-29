1ead40e269f390c839fa1a6ebe2c0d9b9f70e3b8
qqlidakai <qqlidakai@163.com>
2019-05-29 21:45:09 +0800

    idea tijiao

--- Demo/src/cn/com/nantin/Demo3.java
+++ Demo/src/cn/com/nantin/Demo3.java
@@ -3,6 +3,6 @@ package cn.com.nantin;
 public class Demo3 {
 public static void main(String[] args) {
 	System.out.println("eclipse");
-	System.out.println("eclipses");
+	System.out.println("idea");
 }
 }
--- Demo/src/cn/com/nantin/Demo4.java
+++ Demo/src/cn/com/nantin/Demo4.java
@@ -2,6 +2,6 @@ package cn.com.nantin;
 
 public class Demo4 {
 	public static void main(String[] args) {
-		System.out.println("eclipse");
+		System.out.println("idea");
 	}
 }
